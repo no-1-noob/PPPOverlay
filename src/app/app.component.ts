6f43555cda95fbd7ffd93b9716b5ee888bbb3af7
No.1 N[][]b <91905916+no-1-noob@users.noreply.github.com>
2025-12-13 13:50:59 +0100

    v21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
-    imports: [CommonModule, RouterOutlet],
+    imports: [CommonModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
+	standalone: true
 })
 export class AppComponent implements AfterViewInit {
 	title = 'PPPOverlay';
